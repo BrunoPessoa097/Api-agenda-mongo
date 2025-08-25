eb78137818a5e27dd1310c3e120185721b529f89
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-08-25 22:28:21 +0000

    Adicionando comentários no file server

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import dotenv from 'dotenv';
 import app from './routes/app';
 import conn from './config/db/mongo';
 
-
+// Importando dotenv.
 dotenv.config();
 const env = process.env;
 
+// Configurando a porta do servidor.
 const PORT: number = parseInt(`${env.PORT || 5000}`);
 
+// Iniciando o servidor.
 app.listen(PORT, () => {
   console.log('Servidor rodando!');
   console.log(`Porta: ${PORT}`);
+  // Iniciando o banco de dados.
   conn();
 })
