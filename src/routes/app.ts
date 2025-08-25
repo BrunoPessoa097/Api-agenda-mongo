60881ca2d666196e8073d40acf7274a46db67b81
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-08-25 22:03:34 +0000

    Adicionando comentários e padronizando file app.
    - Adicionando comentários.
    - Padronização dos imports.

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,30 +1,38 @@
 import express, { Request, Response } from 'express';
-import pessoaRouter from './pessoaRoutes';
 import helmet from 'helmet';
 import cors from 'cors';
 import permissionsPolicy from 'permissions-policy';
+
+// imports de arquivos criados.
 import politics from '../config/politics';
+import pessoaRouter from './pessoaRoutes';
 
 const app = express();
+
+// Configurando os middlewares.
 app.use(express.json());
 app.use(helmet ());
 app.use(cors({
-  origin: 'http://localhost',
-  methods: ['GET','POST','PUT','DELETE']
+  origin: 'http://localhost', // Para adicionar novas origens transformad num array.
+  methods: ['GET','POST','PUT','DELETE'] // Métodos permitidos.
 }));
+// Políticas de privacidade.
 app.use(permissionsPolicy({
   features: politics
 }))
 
+// Importando rotas.
 app.use(pessoaRouter);
 
 app.get('/', (req: Request, res: Response)=>{
-   res.status(200).json({
+   // Rota padrão.
+  res.status(200).json({
      message: 'Rota principal'
    });
 });
 
 app.use((req: Request, res: Response)=>{
+  // Rota Default.
   res.status(404).json({
     message: 'Rota não encontrada'
   });
